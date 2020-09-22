e9d644dd7e596bb688b39af87f606ed484e05f4a
arn-the-long-beard <arn.menant@gmail.com>
2020-09-22 18:43:02 +0200

    fix: corrected equality check for route

--- examples/custom_router/src/router/route.rs
+++ examples/custom_router/src/router/route.rs
@@ -50,7 +50,14 @@ impl Clone for Route {
 
 impl PartialEq for Route {
     fn eq(&self, other: &Self) -> bool {
-        self.path == other.path && self.name == other.name && self.children == other.children
+        if self.url.is_none() && other.url.is_none() {
+            self.path == other.path && self.name == other.name && self.children == other.children
+        } else {
+            self.url.clone().unwrap() == other.url.clone().unwrap()
+                && self.path == other.path
+                && self.name == other.name
+                && self.children == other.children
+        }
     }
 }
 #[cfg(test)]
