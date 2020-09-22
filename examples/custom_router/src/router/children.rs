b4a13d9f43113b957754417229772cffcecb4062
arn-the-long-beard <arn.menant@gmail.com>
2020-09-22 16:59:52 +0200

    feature: added get_default_route() on ExtractRoute trait

--- examples/custom_router/src/router/children.rs
+++ examples/custom_router/src/router/children.rs
@@ -4,4 +4,13 @@ use std::collections::HashMap;
 pub trait ExtractRoutes {
     fn get_routes() -> Vec<Route>;
     fn get_hashed_routes() -> HashMap<String, Route>;
+    fn get_default_route() -> Route {
+        let routes = Self::get_routes();
+        let default_route = routes.iter().find(|r| r.default);
+        if default_route.is_none() {
+            panic!("You need a default route for your routing to redirect when wrong url/path");
+        }
+        let def = default_route.unwrap().clone();
+        def.clone()
+    }
 }
